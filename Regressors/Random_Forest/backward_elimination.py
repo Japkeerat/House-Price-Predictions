1db394f57b7917213eea92ff219babf303e169f7
Japkeerat Singh <japkeerat21@gmail.com>
2019-01-04 17:50:11 +0530

    Add files via upload

---
+++ Regressors/Random_Forest/backward_elimination.py
@@ -0,0 +1,49 @@
+"""
+Feature Selection using Backward Elimination property and hence building a 
+model on the selected features.
+"""
+
+import statsmodels.formula.api as sm
+import numpy as np
+from sklearn.model_selection import train_test_split as tts
+from sklearn.ensemble import RandomForestRegressor
+
+def be_rfr(features, target):
+    features = add_ones(features)
+    features = find_optimal_features(features, target)
+    X_train, X_test, y_train, y_test = tts(features, target, test_size = 0.2)
+    y_predict = build_model(X_train, X_test, y_train)
+    test_pred = zip(y_test, y_predict)
+    return test_pred
+    
+
+# Adding an array of 1 as column matrix to the features matrix to avoid the
+# limitation of backward elimination
+def add_ones(features):
+    features = np.append(arr = np.ones((len(features), 1), dtype = int), values = features, axis = 1)
+    return features
+
+
+# Finding the columns that affect the most on model. This is kind of preprocessing
+# work for model building. It returns the list of indexes of columns that have
+# the most affect which is determined by the p-value of the column.
+def find_optimal_features(features, target):
+    significance_level = 0.05
+    size = len(features[0])
+    for i in range(0, size):
+        reg_ols = sm.OLS(endog = target, exog = features).fit()
+        max_var = max(reg_ols.pvalues).astype(float)
+        if(max_var>significance_level):
+            for j in range(0, size - i):
+                if(reg_ols.pvalues[j].astype(float) == max_var):
+                    features = np.delete(features, j, 1)
+    return features
+
+
+# This builds the Linear Regression model and also predicts values for some
+# section of features
+def build_model(X_train, X_test, y_train):
+    regressor = RandomForestRegressor(n_estimators = 750, criterion = "mae", warm_start = True)
+    regressor.fit(X_train, y_train)
+    y_predict = regressor.predict(X_test)
+    return y_predict
--- main_file.py
+++ main_file.py
@@ -4,7 +4,11 @@ from Preprocessing import dataframe as df # Custom class
 from Preprocessing import cleansing as cl # Custom class
 from sklearn.model_selection import train_test_split
 from Regressors.Random_Forest.all_in import simple_rfr # Custom class
+from Regressors.Random_Forest.backward_elimination import be_rfr # Custom class
+from Regressors.Multiple_Linear.all_in import simple_multiple_lr # Custom class
+from Regressors.Multiple_Linear.backward_elimination import be_multiple_lr # Custom class
 from Postprocessing.Rsquare import adj_R_square # Custom class
+from Postprocessing.accuracy_of_model import get_accuracy
 
 # Importing and reading the csv file for training model.
 # Also dropping the column that contains ID of every row which is practically not useful.
@@ -27,5 +31,24 @@ features = cl.cleanse_features(features)
 # Splitting to training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(features, target, test_size = 0.2)
 
+# Running the algorithm for Random Forest with all in approach
 y_predict = simple_rfr(X_train, X_test, y_train, y_test)
+print(adj_R_square(y_test, y_predict, 79, 1460)) # 0.84944
+print(get_accuracy(y_test, y_predict, 10)) # 65.4%
+
+# Running the algorithm for Mutliple Linear Regression with all in approach
+y_predict = simple_multiple_lr(X_train, X_test, y_train, y_test)
 print(adj_R_square(y_test, y_predict, 79, 1460))
+print(get_accuracy(y_test, y_predict, 10))
+
+# Running the algorithm for Linear Regression with backward elimination approach
+y_test_pred = be_multiple_lr(features, target)
+y_test, y_predict = zip(*y_test_pred)
+print(adj_R_square(y_test, y_predict, 79, 1460))
+print(get_accuracy(y_test, y_predict, 10))
+
+# Running the algorithm for Random Forest Regression with backward elimination approach
+y_test_pred = be_rfr(features, target)
+y_test, y_predict = zip(*y_test_pred)
+print(adj_R_square(y_test, y_predict, 79, 1460))
+print(get_accuracy(y_test, y_predict, 10))
